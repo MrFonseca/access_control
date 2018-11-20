d9b66f8799bd67888228adc1f7432f1a48497f1a
MrFonseca <fredidf@gmail.com>
2018-11-20 09:39:03 -0300

    Implement House views
    - Implement RESTful actions in houses controller;
    - Refact houses index view;
    - Implement form partial for houses;
    - Create show and edit views;

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,23 +1,49 @@
 class HousesController < ApplicationController
+  before_action :set_house, only: %i[edit show update]
+
   def index
     @houses = House.all
+  end
 
+  def new
     @house = House.new
   end
 
   def create
-    House.create(house_params)
-    redirect_to "/houses"
+    @house = House.new(house_params)
+
+    if @house.save
+      redirect_to @house
+    else
+      render "new"
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @house.update(house_params)
+      redirect_to @house
+    else
+      render "edit"
+    end
   end
 
   def destroy
-    House.find(params[:id]).destroy
-    redirect_to "/houses"
+    @house = House.find(params[:id])
+    @house.destroy
+    redirect_to houses_path
   end
 
   private
 
   def house_params
     params.require(:house).permit(:number, :address, :route, :status)
+  end
+
+  def set_house
+    @house = House.find(params[:id])
   end
 end
