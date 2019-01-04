8d24c6ff3e9adff15db521472041b8b427b16c77
MrFonseca <fredidf@gmail.com>
2019-01-04 13:58:33 -0200

    Remove access column from people table
    - Add migration to remove the access column from people table;
    - Refact seeds file;

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181106193812) do
+ActiveRecord::Schema.define(version: 2019_01_04_155420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 20181106193812) do
     t.string "name"
     t.string "document"
     t.string "phone"
-    t.boolean "access"
     t.bigint "house_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
