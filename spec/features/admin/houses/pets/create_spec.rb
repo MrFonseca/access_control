43ba777b34c48ce653919737de353e5cdf9f7ee3
MrFonseca <fredidf@gmail.com>
2019-01-11 16:14:34 -0200

    Implement pet creation
    - Implement new and create actions in pet controller;
    - Implement pet form partial;
    - Implement pet new view;
    - Add link to house show from pet index;

---
+++ spec/features/admin/houses/pets/create_spec.rb
@@ -0,0 +1,35 @@
+RSpec.describe "creating a new pet", type: :feature do
+  let!(:house) { FactoryBot.create(:house, number: "114") }
+
+  before do
+    admin_login
+    click_on "114"
+    click_on "Add pet"
+    click_on "New pet"
+  end
+
+  context "with valid input" do
+    it "creates a new pet" do
+      fill_in  "Name",    with: "Scooby-Doo"
+      fill_in  "Species", with: "Dog"
+      fill_in  "Breed",   with: "Great Dane"
+      fill_in  "Color",   with: "Brown"
+      choose   "Male"
+      choose   "Big"
+      click_on "Submit"
+
+      pet = Pet.order(:created_at).last
+      expect(current_path).to eq(admin_house_pets_path(house))
+      expect(page).to have_content("Pet created")
+      expect(pet).to have_attributes(
+        name:     "Scooby-Doo",
+        species:  "Dog",
+        breed:    "Great Dane",
+        color:    "Brown",
+        gender:   "Male",
+        size:     "Big",
+        house_id: house.id
+      )
+    end
+  end
+end
